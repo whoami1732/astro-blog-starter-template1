33169e74c8a80b97a7700fba41f8e18b38ed6d94
whoami1732 <whoami1731@139.com>
2026-08-12 16:05:46 +0800

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "灵峰科技 Blog";
+export const SITE_DESCRIPTION = "欢迎来到灵峰科技网站博客!";
